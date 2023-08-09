ab8bc123cd45e3652d0c3625da8731ed615b28b1
Astaroth <astar0th-9@yandex.ru>
2023-08-09 22:16:16 +0700

    JumpState

--- Assets/Photo/Scripts/Characters/Player/StateBehaviour/JumpState.cs
+++ Assets/Photo/Scripts/Characters/Player/StateBehaviour/JumpState.cs
@@ -1,29 +1,22 @@
-using Photo;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class JumpState : StateMachineBehaviour
+namespace Photo
 {
-    [SerializeField] private LayerMask _ground;
-    
-    public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    public class JumpState : BaseState
     {
-        var isGround = Physics2D.OverlapCircle(animator.gameObject.transform.parent.position, 0.1f, _ground);
-            
-        if (isGround)
+        public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
         {
-            animator.SetBool("Jump", false);
+            animator.SetBool(RUN, false);
+        }
+
+        public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+        }
+
+        public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            if (Player.GroundChecker.Check())
+                animator.SetBool(JUMP, false);
         }
     }
-
-    // OnStateMove is called right after Animator.OnAnimatorMove()
-    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    // Implement code that processes and affects root motion
-    //}
-
-    // OnStateIK is called right after Animator.OnAnimatorIK()
-    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    // Implement code that sets up animation IK (inverse kinematics)
-    //}
 }
