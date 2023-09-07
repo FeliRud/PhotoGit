152b9b5f56bbc7c94d4b0773d8752330b8116f5c
Astaroth <astar0th-9@yandex.ru>
2023-09-08 00:55:10 +0700

    Select end option.

---
+++ Assets/Photo/Scripts/Scenes/End/EndWindow.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.UI;
+
+namespace Photo.End
+{
+    public class EndWindow : MonoBehaviour
+    {
+        [SerializeField] private GameObject[] _startItems;
+        
+        [Header("First Option")] 
+        [SerializeField] private PlayableDirector _firstPlayable;
+        [SerializeField] private GameObject[] _firstItems;
+        [SerializeField] private Button _firstOptionButton;
+
+        [Header("Second Option")]
+        [SerializeField] private PlayableDirector _secondPlayable;
+        [SerializeField] private GameObject[] _secondItems;
+        [SerializeField] private Button _secondOptionButton;
+
+        private void OnEnable()
+        {
+            _firstOptionButton.onClick.AddListener(FirstOptionButtonClicked);
+            _secondOptionButton.onClick.AddListener(SecondOptionButtonClicked);
+        }
+
+        private void OnDisable()
+        {
+            _firstOptionButton.onClick.AddListener(FirstOptionButtonClicked);
+            _secondOptionButton.onClick.AddListener(SecondOptionButtonClicked);
+        }
+
+        private void FirstOptionButtonClicked()
+        {
+            CloseWindow();
+            CloseItems(_startItems);
+            OpenItems(_firstItems);
+            PlayCutscene(_firstPlayable);
+        }
+
+        private void SecondOptionButtonClicked()
+        {
+            CloseWindow();            
+            CloseItems(_startItems);
+            OpenItems(_secondItems);
+            PlayCutscene(_secondPlayable);
+        }
+
+        private void OpenItems(GameObject[] items)
+        {
+            foreach (var item in items) 
+                item.SetActive(true);
+        }
+
+        private void CloseItems(GameObject[] items)
+        {
+            foreach (var item in items) 
+                item.SetActive(false);
+        }
+
+        private void CloseWindow() => 
+            gameObject.SetActive(false);
+
+        private void PlayCutscene(PlayableDirector playableDirector) => 
+            playableDirector.Play();
+    }
+}
