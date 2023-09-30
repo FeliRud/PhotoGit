0ff2d0076f687d8e5542eca8bbd153c744381ab2
Astaroth <astar0th-9@yandex.ru>
2023-09-30 15:10:22 +0700

    Small changed

--- Assets/Photo/Scripts/AudioChanger.cs
+++ Assets/Photo/Scripts/AudioChanger.cs
@@ -3,24 +3,24 @@ using UnityEngine.Audio;
 
 namespace Photo
 {
-    public class AudioChanger : MonoBehaviour
+  public class AudioChanger : MonoBehaviour
+  {
+    [SerializeField] private AudioSource _audioSource;
+    [SerializeField] private AudioMixer _masterMixer;
+
+    public void SetMusic(AudioClip audio)
     {
-        [SerializeField] private AudioSource _audioSource;
-        [SerializeField] private AudioMixer _masterMixer;
+      if (_audioSource.clip == audio)
+        return;
 
-        public void SetMusic(AudioClip audio)
-        {
-            if (_audioSource.clip == audio) 
-                return;
-            
-            _audioSource.clip = audio;
-            _audioSource.Play();
-        }
-
-        public void ChangeMusicValue(float value)
-        {
-            float volume = Mathf.Lerp(-80, 0, value);
-            _masterMixer.SetFloat("Music", volume);
-        }
+      _audioSource.clip = audio;
+      _audioSource.Play();
     }
+
+    public void ChangeMusicValue(float value)
+    {
+      float volume = Mathf.Lerp(-80, 0, value);
+      _masterMixer.SetFloat("Music", volume);
+    }
+  }
 }
